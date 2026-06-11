e864ac657ba9141a60af6fb81affddb6b98d2ae3
Lakshayy2406 <lakshayvaishnav2406@gmail.com>
2026-06-12 03:50:04 +0530

    fix blank columns

--- backend/expense.py
+++ backend/expense.py
@@ -6,7 +6,7 @@ expense_bp=Blueprint("expense",__name__)
 @expense_bp.post("/add")
 def add():
  d=request.json
- get_sheet("Expense").append_row([datetime.now().strftime("%d-%m-%Y"),d["title"],"",d["amount"],d["mode"]])
+ get_sheet("Expense").append_row([datetime.now().strftime("%d-%m-%Y"),d["title"],d["amount"],d["mode"]])
  return {"success":True}
 @expense_bp.get("/all")
 def all(): return jsonify(get_sheet("Expense").get_all_records())
