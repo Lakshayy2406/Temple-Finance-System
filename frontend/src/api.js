3a578f2a68eb76c7f91d4ca4b9268ee5f196841f
Lakshayy2406 <lakshayvaishnav2406@gmail.com>
2026-06-12 02:38:23 +0530

    connect live backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const api = axios.create({ baseURL: "http://127.0.0.1:5000" });
+const api = axios.create({ baseURL: "https://temple-finance-backend.onrender.com" });
 
 export const fetchIncome = () => api.get("/income/all");
 export const fetchExpense = () => api.get("/expense/all");
